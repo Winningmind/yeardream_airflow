bda4a09015f68686d7e860e93e839cae4fe1ba6d
Winningmind <yozo2@naver.com>
2024-06-17 02:42:54 +0900

    sh 명령으로 실행

--- dags/dags_bash_select_fruit.py
+++ dags/dags_bash_select_fruit.py
@@ -11,12 +11,12 @@ with DAG(
     
     t1_orange = BashOperator(
         task_id="t1_orange",
-        bash_command="/opt/airflow/plugins/shell/select_fruit.sh ORANGE", # 워커 컨테이너의 경로
+        bash_command="sh /opt/airflow/plugins/shell/select_fruit.sh ORANGE", # 워커 컨테이너의 경로
     )
 
     t2_avocado = BashOperator(
         task_id="t2_avocado",
-        bash_command="/opt/airflow/plugins/shell/select_fruit.sh AVOCADO", # 워커 컨테이너의 경로
+        bash_command="sh /opt/airflow/plugins/shell/select_fruit.sh AVOCADO", # 워커 컨테이너의 경로
     )
 
     t1_orange >> t2_avocado
